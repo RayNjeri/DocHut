835fc5d67bd335d8d2d5b27a531e0fdeaa3d6013
Rachael Njeri <rachael.njeri@andela.com>
2017-06-09 08:16:52 +0300

    add document tests
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

---
+++ server/tests/document.test.js
@@ -0,0 +1,37 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const supertest = require('supertest');
+const should = require('chai').should;
+expect = require('chai').expect;
+chai.use(chaiHttp);
+
+api = supertest('http://localhost:8000');
+
+describe('Document', () => {
+  it('should create a document on /document/POST', (done) => {
+    api.get('/api/document/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(response.statusMessage).to.equal('Document Successfully Created');
+      expect(body).to.equal(true);
+    });
+    done();
+  });
+
+  it('shoould update a document on /document/PUT', (done) => {
+    api.put('/api/document/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(response.statusMessage).to.equal('Document successfully Updated');
+      expect(body).to.equal(true);
+    });
+    done();
+  });
+
+  it('should delete a document on /document/DELETE', (done) => {
+    api.delete('/api/document/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(response.statusMessage).to.equal('Document Successfully Deleted');
+      expect(response.body.name).to.not.equal(null);
+    });
+  });
+
+});
