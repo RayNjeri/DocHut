bc0474eee55192dd59733624d5fd9b4026458930
Rachael Njeri <rachael.njeri@andela.com>
2017-06-12 14:58:39 +0300

    set email as unique
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- server/models/user.js
+++ server/models/user.js
@@ -16,8 +16,21 @@ module.exports = (sequelize, DataTypes) => {
     },
     email: {
       type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
+      unique: {
+        args: true,
+        msg: 'Oops. Looks like you already have an account with this email address. Please try to login.',
+        fields: [sequelize.fn('lower', sequelize.col('email'))]
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'The email you entered is invalid or is already in our system.'
+        },
+        max: {
+          args: 254,
+          msg: 'The email you entered is invalid or longer than 254 characters.'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
