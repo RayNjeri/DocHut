0cb76e5d9915f26d06f857e4e88c8a88e037117b
Rachael Njeri <rachael.njeri@andela.com>
2017-06-15 13:10:59 +0300

    instantiate store and provider
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import configureStore from './store/ConfigureStore';
+import {loadUsers} from './actions/userActions';
 import routes from './routes';
+
+const store = configureStore();
+
+store.dispatch(loadUsers());
 
 render(
   <Provider store={store}>
--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import documents from './documentReducer';
 
 
 const reducers = combineReducers({
+  documents
+
 
 });
 
