12dd2b6970d5f89fc0e07fe27d07bf1e2ca62e81
Rachael Njeri <rachael.njeri@andela.com>
2017-06-23 11:56:15 +0300

    add post,put,delete and get methods to userActions
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -82,4 +82,82 @@ export const usersSearchFilter = searchFilter => ({
   searchFilter
 });
 
+/* eslint no-undef: "off"*/
+
+export const listUsers = () => (dispatch) => {
+  dispatch(usersRequest());
+  return (
+    request
+      .get('/api/user')
+      .set('x-access-token', window.localStorage.getItem('token'))
+      .then((response) => {
+        dispatch(usersSuccess(response.body));
+      })
+      .catch((error) => {
+        dispatch(usersFailure(error));
+      })
+  );
+};
+
+export const createUser = userData => (dispatch) => {
+  dispatch(usersAddRequest(userData));
+  return (
+    request
+      .post('/api/user')
+      .set('x-access-token', window.localStorage.getItem('token'))
+      .send(userData)
+      .then((response) => {
+        dispatch(usersAddSuccess(response.body));
+      })
+      .catch((error) => {
+        dispatch(usersAddFailure(error.response));
+      })
+  );
+};
+
+export const updateUser = userData => (dispatch) => {
+  dispatch(usersUpdateRequest(userData));
+  return (
+    request
+      .put(`/api/user/${userData.id}`)
+      .set('x-access-token', window.localStorage.getItem('token'))
+      .send(userData)
+      .then((response) => {
+        dispatch(usersUpdateSuccess(response.body));
+      })
+      .catch((error) => {
+        dispatch(usersUpdateFailure(error.response));
+      })
+  );
+};
+
+export const deleteUser = userId => (dispatch) => {
+  dispatch(usersDeleteRequest());
+  return (
+    request
+      .delete(`/api/user/${userId}`)
+      .set('x-access-token', window.localStorage.getItem('token'))
+      .then((response) => {
+        dispatch(usersDeleteSuccess(response.body));
+      })
+      .catch((error) => {
+        dispatch(usersDeleteFailure(error.response));
+      })
+  );
+};
+
+export const getUser = userId => (dispatch) => {
+  dispatch(usersGetRequest());
+  return (
+    request
+      .get(`/api/user/${userId}`)
+      .set('x-access-token', window.localStorage.getItem('token'))
+      .then((response) => {
+        dispatch(usersGetSuccess(response.body));
+      })
+      .catch((error) => {
+        dispatch(usersGetFailure(error.response));
+      })
+  );
+};
 
