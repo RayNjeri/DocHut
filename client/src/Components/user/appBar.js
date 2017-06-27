1ca231b44fcd095c5e479c6b6bb37ce86ae8c399
Rachael Njeri <rachael.njeri@andela.com>
2017-06-27 17:36:43 +0300

    fix users display
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- client/src/Components/user/appBar.js
+++ client/src/Components/user/appBar.js
@@ -11,75 +11,75 @@ import Subheader from 'material-ui/Subheader';
 
 
 const NavBar = (props) => {
-  const upperCaseFirst = (name) => {
-    return name.replace(/[a-z]/, name[0].toUpperCase());
-  };
+    const upperCaseFirst = (name) => {
+        return name.replace(/[a-z]/, name[0].toUpperCase());
+    };
 
-  return (
-    <div>
-      <AppBar
-        title="DocHut"
-        titleStyle={{ color: 'white' }}
-        iconStyleLeft={{ color: 'white' }}
-        onLeftIconButtonTouchTap={props.openDrawer}
-        iconElementRight={
-          <span>
-            <FlatButton label="LogOut" onClick={props.onLogOut}
-              style={{ color: props.muiTheme.palette.alternateTextColor }} />
-          </span>
-        }
-      />
+    return (
+        <div>
+            <AppBar
+                title="DocHut"
+                titleStyle={{ color: 'white' }}
+                iconStyleLeft={{ color: 'white' }}
+                onLeftIconButtonTouchTap={props.openDrawer}
+                iconElementRight={
+                    <span>
+                        <FlatButton label="LogOut" onClick={props.onLogOut}
+                            style={{ color: props.muiTheme.palette.alternateTextColor }} />
+                    </span>
+                }
+            />
 
-      <Drawer
-        docked={false}
-        width={300}
-        open={props.isOpen.open}
-        onRequestChange={props.onClose}
-      >
-        <div style={{ backgroundColor: props.muiTheme.palette.primary1Color }}>
-          <br />
+            <Drawer
+                docked={false}
+                width={300}
+                open={props.isOpen.open}
+                onRequestChange={props.onClose}
+            >
+                <div style={{ backgroundColor: props.muiTheme.palette.primary1Color }}>
+                    <br />
+                    <p style={{
+                        margin: 12, fontSize: 14, fontWeight: 500,
+                        color: props.muiTheme.palette.alternateTextColor
+                    }} /> : <p style={{
+                        margin: 12, fontSize: 14, fontWeight: 500,
+                        color: props.muiTheme.palette.alternateTextColor
+                    }} />
+                    }
           <p style={{
-            margin: 12, fontSize: 14, fontWeight: 500,
-            color: props.muiTheme.palette.alternateTextColor
-          }} /> : <p style={{
-            margin: 12, fontSize: 14, fontWeight: 500,
-            color: props.muiTheme.palette.alternateTextColor
-          }} />
-          }
-          <p style={{
-            margin: 12, fontSize: 14, fontWeight: 500,
-            color: props.muiTheme.palette.alternateTextColor
-          }} />
-          <br />
+                        margin: 12, fontSize: 14, fontWeight: 500,
+                        color: props.muiTheme.palette.alternateTextColor
+                    }} />
+                    <br />
+                </div>
+                <Divider />
+                <List>
+                    <Subheader>View users:</Subheader>
+                    {props.users && props.users.length ?
+                        props.users.map((user) => (
+                            <ListItem
+                                key={user.id}
+                                primaryText={user.userName}
+                                onTouchTap={() => props.onSelectUser(user)}
+                            />
+                        ))
+                        : <span> No users found </span>
+                    }
+                </List>
+            </Drawer>
         </div>
-        <Divider />
-        <List>
-          <Subheader>View users:</Subheader>
-          {props.users && props.users.length ?
-            props.users.map((user) => (
-              <ListItem
-                key={user.id}
-                primaryText={user.username}
-                onTouchTap={() => props.onSelectUser(user)}
-              />
-            ))
-            : <span> No users found </span>
-          }
-        </List>
-      </Drawer>
-    </div>
-  );
+    );
 };
 
 NavBar.propTypes = {
-  authReducer: PropTypes.object,
-  users: PropTypes.array,
-  muiTheme: PropTypes.object,
-  openDrawer: PropTypes.func,
-  onClose: PropTypes.func,
-  onLogOut: PropTypes.func,
-  onSelectUser: PropTypes.func,
-  isOpen: PropTypes.object
+    authReducer: PropTypes.object,
+    users: PropTypes.array,
+    muiTheme: PropTypes.object,
+    openDrawer: PropTypes.func,
+    onClose: PropTypes.func,
+    onLogOut: PropTypes.func,
+    onSelectUser: PropTypes.func,
+    isOpen: PropTypes.object
 };
 
 
--- client/src/Components/user/userView.js
+++ client/src/Components/user/userView.js
@@ -10,81 +10,84 @@ import * as userActions from '../../actions/userActions';
 import * as authActions from '../../actions/authActions';
 
 class UserContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false
-    };
-    this.handleClose = this.handleClose.bind(this);
-    this.handleOpen = this.handleOpen.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleLogOut = this.handleLogOut.bind(this);
-  }
-  componentDidMount() {
-    this.props.userActions.listUsers().then(() => {
-    });
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+        this.handleClose = this.handleClose.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleLogOut = this.handleLogOut.bind(this);
+    }
+    componentDidMount() {
+        this.props.userActions.listUsers().then(() => {
+        });
+    }
 
-  handleLogOut() {
-    this.props.authActions.logoutUser();
-    this.context.router.push('/home');
-  }
+    handleLogOut() {
+        this.props.authActions.logoutUser();
+        this.context.router.push('/home');
+    }
 
-  handleClose() {
-    this.setState({
-      open: false
-    });
-  }
+    handleClose() {
+        this.setState({
+            open: false
+        });
+    }
 
-  handleSelect(user) {
-    this.props.userActions(user).then(() => {
-      this.context.router.push(`/profile/${user.id}`);
-    });
-  }
+    handleSelect(user) {
+        this.props.userActions(user).then(() => {
+            this.context.router.push(`/profile/${user.id}`);
+        });
+    }
 
-  handleOpen() {
-    this.setState({
-      open: true
-    });
-  }
+    handleOpen() {
+        this.setState({
+            open: true
+        });
+    }
 
-  render() {
-    return (
-      this.props.users.loading ?
-        <CircularProgress size={60} thickness={5} /> :
-        <NavBar openDrawer={this.handleOpen}
-          isOpen={this.state}
-          onClose={this.handleClose}
-          onLogOut={this.handleLogOut}
-          onSelectUser={this.handleSelect}
-        />
-    );
-  }
+    render() {
+        return (
+            this.props.users.loading ?
+                <CircularProgress size={60} thickness={5} /> :
+                <NavBar
+                    openDrawer={this.handleOpen}
+                    users={this.props.users}
+                    isOpen={this.state}
+                    onClose={this.handleClose}
+                    onLogOut={this.handleLogOut}
+                    onSelectUser={this.handleSelect}
+                />
+        );
+    }
 }
 
 UserContainer.propTypes = {
-  userActions: PropTypes.object.isRequired,
-  users: PropTypes.object,
-  authReducer: PropTypes.object,
-  authActions: PropTypes.object,
+    userActions: PropTypes.object.isRequired,
+    users: PropTypes.object,
+    authReducer: PropTypes.object,
+    authActions: PropTypes.object,
 };
 
 UserContainer.contextTypes = {
-  router: PropTypes.object
+    router: PropTypes.object
 };
 
 function mapStateToProps(state) {
-  return {
-    users: state.users,
-    authReducer: state.authReducer
-  };
+    console.log('This is the store', state);
+    return {
+        users: state.users.users,
+        authReducer: state.authReducer
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    userActions: bindActionCreators(userActions, dispatch),
-    authActions: bindActionCreators(authActions, dispatch)
-  };
+    return {
+        userActions: bindActionCreators(userActions, dispatch),
+        authActions: bindActionCreators(authActions, dispatch)
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
