1058f223d8a646c5dff378d9b455a28443d388d6
Rachael Njeri <rachael.njeri@andela.com>
2017-07-05 14:25:04 +0300

    add a debounce event listener to manage interval checks and original listener function calling
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- client/src/Components/document/documentSearch.js
+++ client/src/Components/document/documentSearch.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import _ from 'underscore';
 import * as documentActions from '../../actions/documentsAction';
 
 
@@ -11,13 +12,21 @@ class DocumentSearch extends React.Component {
     this.searchDocument = this.searchDocument.bind(this);
     this.handleSearchInput = this.handleSearchInput.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.apiCall = this.apiCall.bind(this);
+    this.makeSearch = _.debounce(this.apiCall, 500);
   }
   searchDocument(searchFilter) {
     this.props.actions.searchDocument(searchFilter);
   }
+
+  apiCall() {
+    this.props.actions.searchDocument(this.state.searchFilter);
+  }
   handleSearchInput(e) {
     this.setState({ searchFilter: e.target.value });
+    this.makeSearch();
   }
+
   onSubmit(e) {
     e.preventDefault();
     this.searchDocument(this.state.searchFilter);
@@ -29,20 +38,15 @@ class DocumentSearch extends React.Component {
           id="search"
           onChange={this.handleSearchInput}
         />
-        {/*<a
-          className="btn-floating btn-large waves-effect waves-light red">*/}
         <i className="material-icons" onClick={(this.onSubmit)} >search</i>
       </div>
 
     );
   }
 }
-// Props validation
 DocumentSearch.propTypes = {
   actions: PropTypes.object.isRequired
 };
-
-// const mapStateToProps = (state, ownProps) => ({ searchFilter: state.searchFilter });
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(documentActions, dispatch)
